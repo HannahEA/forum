e3cc7533cf86c586bacbd0779de79d8961649055
Your Name <n.d.russell@hotmail.com>
2022-05-02 18:32:38 +0100

    all

--- main.go
+++ main.go
@@ -2,24 +2,33 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var sqliteDatabase *sql.DB
+
 func main() {
 
 	//Open the database SQLite file and create the database table
-	sqliteDatabase, err1 := sql.Open("sqlite3", "database/sqlite-database.db")
+	database, err1 := sql.Open("sqlite3", "sqlite-database.db")
+	sqliteDatabase = database
+
 	if err1 != nil {
 		log.Fatal(err1.Error())
 	}
 	//Defer the close
 	defer sqliteDatabase.Close()
 
-	http.HandleFunc("/", registration)
+	fmt.Println(LoginValidator("helld@hotmail.com", "rew", sqliteDatabase))
+	fmt.Println(LoginValidator("helld@hotil.com", "HelloW", sqliteDatabase))
+
+	http.HandleFunc("/", LoginHandler)
+	http.HandleFunc("/register", registration)
+	http.HandleFunc("/registration", registration2)
 	http.ListenAndServe(":8080", nil)
 
-	newUser("ndr@hotmail.com", "nater68", "HelloW", sqliteDatabase)
 }
--- registration.go
+++ registration.go
@@ -14,15 +14,10 @@ func newUser(email, username, password string, db *sql.DB) {
 		fmt.Printf("The error is %v", errNewUser.Error())
 		log.Fatal()
 	}
-
-	// _, preAdded := add.Exec(email, username, password)
-	// if preAdded != nil {
-	// 	log.Fatal(preAdded.Error())
-	// }
 }
 
 //userExsists checks if the username entered is already taken. If it is the function returns true.
-func userExist(email, username string, db *sql.DB) bool {
+func userExist(email, username string, db *sql.DB) (bool, string) {
 	rows, err := db.Query("SELECT email FROM users WHERE email = ?", email)
 	if err != nil {
 		log.Fatal(err.Error())
@@ -35,7 +30,7 @@ func userExist(email, username string, db *sql.DB) bool {
 
 	rows1, err1 := db.Query("SELECT username FROM users WHERE username = ?", username)
 	if err1 != nil {
-		log.Fatal(err.Error())
+		log.Fatal(err1.Error())
 	}
 
 	count1 := 0
@@ -43,16 +38,92 @@ func userExist(email, username string, db *sql.DB) bool {
 		count1++
 	}
 
-	if count1 == 0 && count == 0 {
+	if count == 0 && count1 == 0 {
+		return true, "You have successfully registered"
+	} else if count1 == 1 && count == 1 {
+		return false, "This account already exists. Please sign in"
+
+	} else if count == 1 {
+		return false, "This email is already taken. Please try a different email address"
+	} else {
+		return false, "This username is already taken. Please try a different username"
+	}
+}
+
+func ValidEmail(email string, db *sql.DB) bool {
+	rows, err := db.Query("SELECT email FROM users WHERE email = ?", email)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	count := 0
+
+	for rows.Next() {
+		count++
+	}
+
+	if count == 0 {
 		return false
 	} else {
 		return true
 	}
 }
+
+func LoginValidator(email, password string, db *sql.DB) bool {
+	rows1, err1 := db.Query("SELECT username FROM users WHERE email = ? AND password = ?", email, password)
+	if err1 != nil {
+		log.Fatal(err1.Error())
+	}
+
+	x := 0
+	for rows1.Next() {
+		x++
+	}
+
+	if x != 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseGlob("templates/login.html"))
+	if err := tpl.Execute(w, ""); err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
 func registration(w http.ResponseWriter, r *http.Request) {
 	tpl := template.Must(template.ParseGlob("templates/register.html"))
-	if err := tpl.Execute(w,"HEllo"); err != nil {
+	if err := tpl.Execute(w, ""); err != nil {
 		log.Fatal(err.Error())
+	}
+}
+
+func registration2(w http.ResponseWriter, r *http.Request) {
+
+
+	userN := r.FormValue("username")
+	email := r.FormValue("email")
+	pass := r.FormValue("password")
+
+
+	exist, value := userExist(email, userN, sqliteDatabase)
+
+	tpl := template.Must(template.ParseGlob("templates/register2.html"))
+
+	if !exist {
+		if err := tpl.Execute(w, value); err != nil {
+			log.Fatal(err.Error())
+		}
+	} else {
+
+		newUser(email, userN, pass, sqliteDatabase)
+
+		if err := tpl.Execute(w, value); err != nil {
+			log.Fatal(err.Error())
+		}
+
 	}
 
 }
